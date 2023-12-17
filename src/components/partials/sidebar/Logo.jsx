d9521e9110f48dc2afa2470c15a6b120bac6dff2
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2023-12-17 09:23:32 -0500

    💀 Backup

--- src/components/partials/sidebar/Logo.jsx
+++ src/components/partials/sidebar/Logo.jsx
@@ -21,11 +21,10 @@ const SidebarLogo = ({ menuHover }) => {
     <div
       className={` logo-segment flex justify-between items-center bg-white dark:bg-slate-800 z-[9] py-6  px-4 
       ${menuHover ? "logo-hovered" : ""}
-      ${
-        skin === "bordered"
+      ${skin === "bordered"
           ? " border-b border-r-0 border-slate-200 dark:border-slate-700"
           : " border-none"
-      }
+        }
       
       `}
     >
@@ -39,13 +38,15 @@ const SidebarLogo = ({ menuHover }) => {
             )}
           </div>
 
-          {(!collapsed || menuHover) && (
-            <div>
-              <h1 className="text-xl font-semibold text-slate-900 dark:text-slate-100">
-                SciCollab
-              </h1>
-            </div>
-          )}
+          {(!collapsed || menuHover)
+            // && (
+            //   <div>
+            //     <h1 className="text-xl font-semibold text-slate-900 dark:text-slate-100">
+            //       SciCollab
+            //     </h1>
+            //   </div>
+            // )
+          }
         </div>
       </Link>
 
@@ -53,11 +54,10 @@ const SidebarLogo = ({ menuHover }) => {
         <div
           onClick={() => setMenuCollapsed(!collapsed)}
           className={`h-4 w-4 border-[1.5px] border-slate-900 dark:border-slate-700 rounded-full transition-all duration-150
-          ${
-            collapsed
+          ${collapsed
               ? ""
               : "ring-2 ring-inset ring-offset-4 ring-black-900 dark:ring-slate-400 bg-slate-900 dark:bg-slate-400 dark:ring-offset-slate-700"
-          }
+            }
           `}
         ></div>
       )}
--- src/components/partials/sidebar/MobileMenu.jsx
+++ src/components/partials/sidebar/MobileMenu.jsx
@@ -49,9 +49,9 @@ const MobileMenu = ({ className = "custom-class" }) => {
               )}
             </div>
             <div>
-              <h1 className="text-xl font-semibold text-slate-900 dark:text-slate-100">
+              {/* <h1 className="text-xl font-semibold text-slate-900 dark:text-slate-100">
                 SciCollab
-              </h1>
+              </h1> */}
             </div>
           </div>
         </Link>
